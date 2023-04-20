c14df10c44edacdd0381b7ff84baa39a2a6ecb22
saiashish8364 <polusaiashish97@gmail.com>
2023-04-20 15:33:31 +0530

    upload1

---
+++ first-test-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Expenseitem from "./components/expenseitems";
+
+function App() {
+  return (
+    <div>
+      <Expenseitem></Expenseitem>
+    </div>
+  );
+}
+
+export default App;
---
+++ first-test-app/src/components/expenseitems.js
@@ -0,0 +1,11 @@
+function expenseitem() {
+  return (
+    <div>
+      <h2>Expense Item</h2>
+      <p>Food Rs 10</p>
+      <p>Petrol Rs 100</p>
+      <p>Movies Rs 200</p>
+    </div>
+  );
+}
+export default expenseitem;
