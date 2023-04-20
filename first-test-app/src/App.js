f9c65f41ff76771cf19970190d904599e2d2cc2c
saiashish8364 <polusaiashish97@gmail.com>
2023-04-20 19:19:55 +0530

    upload2

--- first-test-app/src/App.js
+++ first-test-app/src/App.js
@@ -2,9 +2,46 @@ import "./App.css";
 import Expenseitem from "./components/expenseitems";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      location: "mart",
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      location: "store",
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      location: "online",
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      location: "furniture shop",
+    },
+  ];
   return (
     <div>
-      <Expenseitem></Expenseitem>
+      {expenses.map((item) => (
+        <Expenseitem
+          title={item.title}
+          amount={item.amount}
+          date={item.date}
+          location={item.location}
+        ></Expenseitem>
+      ))}
     </div>
   );
 }
--- first-test-app/src/components/expenseitems.js
+++ first-test-app/src/components/expenseitems.js
@@ -1,11 +1,16 @@
-function expenseitem() {
+import "./expenseitem.css";
+function Expenseitem(props) {
   return (
-    <div>
-      <h2>Expense Item</h2>
-      <p>Food Rs 10</p>
-      <p>Petrol Rs 100</p>
-      <p>Movies Rs 200</p>
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__description">
+          <h2>{props.location}</h2>
+        </div>
+      </div>
+      <div className="expense-item__price">Rs.{props.amount}</div>
     </div>
   );
 }
-export default expenseitem;
+export default Expenseitem;
