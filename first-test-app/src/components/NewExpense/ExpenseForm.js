48d7ba1ee8b1332141667a48c47e573555233627
saiashish8364 <polusaiashish97@gmail.com>
2023-04-29 14:34:38 +0530

    upload9

--- first-test-app/src/components/NewExpense/ExpenseForm.js
+++ first-test-app/src/components/NewExpense/ExpenseForm.js
@@ -4,17 +4,40 @@ const ExpenseForm = () => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
   const callTitle = (event) => {
     setEnteredTitle(event.target.value);
+    // setUserInput((preState) => {
+    //   return { ...preState, enteredTitle: event.target.value };
+    // });
   };
   const callAmount = (event) => {
     setEnteredAmount(event.target.value);
+    // setUserInput((preState) => {
+    //   return { ...preState, enteredAmount: event.target.value };
+    // });
   };
   const callDate = (event) => {
     setEnteredDate(event.target.value);
+    // setUserInput((preState) => {
+    //   return { ...preState, enteredDate: event.target.value };
+    // });
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData);
   };
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
